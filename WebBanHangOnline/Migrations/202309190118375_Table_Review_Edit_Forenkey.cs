ed7999740dbdc9964579117112d35ca4579b8402
Le Thanh Tung <thanhtung070102@gmail.com>
2024-02-21 20:52:37 +0700

    Add project files.

---
+++ WebBanHangOnline/Migrations/202309190118375_Table_Review_Edit_Forenkey.cs
@@ -0,0 +1,20 @@
+namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Table_Review_Edit_Forenkey : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.tb_Review", "ProductId");
+            AddForeignKey("dbo.tb_Review", "ProductId", "dbo.tb_Product", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.tb_Review", "ProductId", "dbo.tb_Product");
+            DropIndex("dbo.tb_Review", new[] { "ProductId" });
+        }
+    }
+}
